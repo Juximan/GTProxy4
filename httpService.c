1e672fcef58ad204732bd1428e425fa2eacf0f91
Juximan <nety.contack@gmail.com>
2025-01-21 13:10:13 +0700

    Add files via upload

--- httpService.c
+++ httpService.c
@@ -18,54 +18,87 @@
 #include "proxyStruct.h"
 
 const unsigned char* certPem = "-----BEGIN CERTIFICATE-----\n\
-MIIDeDCCAmCgAwIBAgIULbUEh/rroH5AIbcdBbMNOGt3uiQwDQYJKoZIhvcNAQEL\n\
-BQAwEDEOMAwGA1UEAwwFZ2l0dT8wHhcNMjMwMTE3MDI1MjAzWhcNMjQwMTE3MDI1\n\
-MjAzWjAQMQ4wDAYDVQQDDAVnaXR1PzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC\n\
-AQoCggEBAMrQhMRtb1v9k7TQ9UkYRMFnvrjcDsfSxPqqNEGmK4lFX2X19o38vMUo\n\
-+68dmBx1j6AAij0grg1sC7K1bZgRkbiOM8DRex3XhT6YO/SsedKOd88QueWbZUnp\n\
-qIOVHiCipMo27AlBR1psUOIhsMiD2MxCdpOeg738NYEii2hhKDW/UXa21nl2DCTQ\n\
-iqgn+6AXM9gVtjsih2Ms2JXJMpGG1Upx3MPjhl7Us8p2K2oVQ3mnYpjDB63mG6Y8\n\
-2IzDX2vLopkP64rMXcSkGMeKMIXRdOeVCK30HqsfLmPSfyA2ye/x/YqfoIS6B/HR\n\
-AKKlwtO1FsduSq2p9G8d2FyaIqcD5tcCAwEAAaOByTCBxjAdBgNVHQ4EFgQUZ1Vl\n\
-461lWuwmgY+Z0/YdGUsDviowHwYDVR0jBBgwFoAUZ1Vl461lWuwmgY+Z0/YdGUsD\n\
-viowDwYDVR0TAQH/BAUwAwEB/zBRBgNVHREESjBIgg5ncm93dG9waWExLmNvbYIS\n\
-d3d3Lmdyb3d0b3BpYTEuY29tgg5ncm93dG9waWEyLmNvbYISd3d3Lmdyb3d0b3Bp\n\
-YTIuY29tMAsGA1UdDwQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcDATANBgkqhkiG\n\
-9w0BAQsFAAOCAQEAO9qTutDwFG6IXKWfh2lzZvIoMK1A0VPZ+x5AP3vBMZJUa+Ae\n\
-LGhvL4i2YaxlzfNndFloymJ5ubIZ1n2NfkeJ+NpcI40Mf02J5H+ltXBlbKsEaA2K\n\
-HOKS6xRtbogJH0JiIG300Nx1T4dXtUSZYMS4Ti5TTrcztTmGK3o5ZLtt8gucpk+G\n\
-O0aIrdPlAyiXXkLF8HwwLTEJ9P4jCdqAacRCW0Pp8op0i1CeBuMVHTGKW4jLav4c\n\
-knSSDgPZplM3Rc5yOMrN0Ff+TgyIKnjxRUlj+SmraCIX9qGfSrd7LMVRAWvaPkGL\n\
-b5uJ4NVYY27+jutHB4TZlB4belxlqNrDECCQCA==\n\
+MIIFHDCCAwQCFAiN1SGWV6ORqkLB3Y2NxzQh/zq2MA0GCSqGSIb3DQEBCwUAMHgx
+CzAJBgNVBAYTAlVTMREwDwYDVQQIDAhOZXcgWW9yazERMA8GA1UEBwwIQnJvb2ts
+eW4xEjAQBgNVBAoMCUludmlzaWJsZTESMBAGA1UECwwJSW52aXNpYmxlMRswGQYD
+VQQDDBJ3d3cuZ3Jvd3RvcGlhMS5jb20wHhcNMjIwOTA4MTM1MjMxWhcNMzIwOTA1
+MTM1MjMxWjAdMRswGQYDVQQDDBJ3d3cuZ3Jvd3RvcGlhMS5jb20wggIiMA0GCSqG
+SIb3DQEBAQUAA4ICDwAwggIKAoICAQCKRK4Ff7RkJ+1IpZWlsLjxZWL3aX0ySiON
+W604PsDi74cNNpLB7PbcFP3o0HieVf2n99+l3jUaVxYQYxAJ3BKtXrTdQ/UWvoYU
+OZowIqnn2NEz7tUh/AxfqkN88z5OQz4MrwSEZ+Oh7hQwGwaFmLazFElwgI6gD8eo
+pLA8fiRk+XKS6dhKs8tR4mDJe1yot67cW5vW6nOGuMyiaDlx6FkeNPjlyQ7BvLjG
+Vt9XDyi1xPBmkPI+GXfAdtGKA7bOhmsvCbh/1gcXKOBeCSk3BpW9ZRnxr9M8lbZt
+lfsu3xX/AbX2g0Z+k3ABnCVcdV01uSZKUKsIzX1r50HmEo3BTn9Og2wYB6AY5YWT
+yGKTAz9V2szjMi/+395iaMLtk2+24/Q6eTCd6vPv8ijwmqNm429VLC2cvhDK2ll3
+sGMu+k7ragm7lLp0ydkaxvUi6IkWMoPks6r9TTwPgfwMuaSN9sb3uz5TlLivOYb1
+088nnMvcXsZ2GA9aBM6R6t8moXcEBobxip7xXQFKNa/w1nUX/d8Z3H9PTUuYwFvY
+8WOZqqSh2hyCFYFvzcSf0jZ9VM5cZxLNJDmrzNo3MCKcAo0KJlzlIDGpkbdevXXC
+NGPoSWlTymwLQZ7F6L9QbJGynLo50gmMGZqz9pLlGRDXqSWEzWciPIbpBhWTCjgl
+xsHiVhtvRQIDAQABMA0GCSqGSIb3DQEBCwUAA4ICAQB4vZYNRrUQ/A1gUbjW3AcI
+0agUPotMx4pcwiZrdXyMant7iSaEXkRiywQLfAtlIzK/Q1IqTTkpWyz5NQGxc6t9
+n3OKWsNjHeJxZbzpp1b1W92bDTcSl7lYoTS5tPF9LUEon+QmnfJaWVS7m+0M87VH
+4q657UMNRXnAzGzNrNrsgQ7xlgJ8btqqJgDYnDTQB1LuUq4jPw6UTAsOYGK9ZoQD
+3wHGosdgVqeQo0y44sMmXh6tAQmN2pP/DTPqE8ML6jfqw3i5QJrKxVPKm9Vxpw67
+R8m3ABgDU1Yiab+I/eGycFs9iMEa+GuXOjh7251EBivNF2XnMXlEWmTAtl3zgM7y
+raG3NGW3EP/eMHA2YjZ6+DsQzPY8xU8cuIxt9gkaNWROKBPolUc5OPMjzjcVftJ3
+7GbLPS8LOswW6mbsjInwO6r/kxH111WLXYx843CJqPRwb2QgoHUMjTdMzzQhKBPK
+QnDRhBHOkbvuFSxhSt4nYEAGzmgvHzLhlsuAvrYzQ2fo7W/LPcusr14zTscF6tNF
+BOypjIFopMCc+joqoTwe55+xZmLrCEAHO99NbI8ZpldRy068tnYBp4vDycf/T7AB
+zjT7/9VlHlaVtuXbhzaTCp0q8DWxKar5nUWUcXsEEImprHI9uq8WPqFI7OPb+vyI
+bNORBaKydcOoQGUrBjHF7g==\n\
 -----END CERTIFICATE-----";
 
 const unsigned char* keyPem = "-----BEGIN PRIVATE KEY-----\n\
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDK0ITEbW9b/ZO0\n\
-0PVJGETBZ7643A7H0sT6qjRBpiuJRV9l9faN/LzFKPuvHZgcdY+gAIo9IK4NbAuy\n\
-tW2YEZG4jjPA0Xsd14U+mDv0rHnSjnfPELnlm2VJ6aiDlR4goqTKNuwJQUdabFDi\n\
-IbDIg9jMQnaTnoO9/DWBIotoYSg1v1F2ttZ5dgwk0IqoJ/ugFzPYFbY7IodjLNiV\n\
-yTKRhtVKcdzD44Ze1LPKditqFUN5p2KYwwet5humPNiMw19ry6KZD+uKzF3EpBjH\n\
-ijCF0XTnlQit9B6rHy5j0n8gNsnv8f2Kn6CEugfx0QCipcLTtRbHbkqtqfRvHdhc\n\
-miKnA+bXAgMBAAECggEBAKUAO+OqvLx0cuTaU5QiIF3Qz4OJ1KSRPxxHuLfoPucw\n\
-/0nub6ZYhiNJEmoHg6czpaOgjNbqbXASBPphTEY5lPo3BrfKNYWzv32LUMvgPkQl\n\
-ECfwDa1VXXRimmxitt4KNFMnl6R37VsNYEh47GBVk92p/NpgTgIU3FqxBgXndVUz\n\
-WpG0KxMcQ32ahIMvoVRZ3jp+DAMTo2h2HEOFC8lciS7bKo5YfXEr3fMUIa57SK0p\n\
-GI81hFBJ1kbHS/RVLZpRxc4CGzUTR3w8eTDzWzHqooV6t31P6t7mskY40cuK3MCV\n\
-2fpYUmxZ8KDYG1hCy/jeQeLN0jvT5CWSkn1leTWYszECgYEA/Jv7wxGi2F6gQvx4\n\
-/NKmrTZxXxJ7+OXWDV9/b0YkW6L83LL6OP+TpsbdMhM7J3nTrhdAfqxvOXVmHeom\n\
-hZK5a94tpDvlq5+1HyGzN1+n3S53JJAQFZdyzj+eOtq4j7xO7JBC4Z9NWz6pqtsD\n\
-xqz05ooxnF0+R+U6EUAm614RfbsCgYEAzYluJPQJwK0WQYRF7dWicqfrvAyy2vwW\n\
-k5+a3Iw7BkK7ZC1b+rt7em7XPoYy1VSZ6sKReEJqr3Jjp9gpb4nGSL7vyxwq3PmT\n\
-xVqb7NeBz6Ynq/DCuPxjg4XvkYYgSDlxmYwo2UMbeA3AjkpQ6GGCSREdHwrqS82R\n\
-CkNthzkJG5UCgYEA+EPtacg5feFyLKOp3QDDI1Vwkvm9sd9hQn9q5VfQf+duERIt\n\
-7jrxSGYGGyLfbmrDakQb9ONP5O7Gz/vCpGCRr7wKq3DaxpQOFGywkGmIt07ldZ/I\n\
-zw+rm5zWzA0OeuWdw838jXZdNqIdangZ+/ccMj/7hiv1+/8D1NRx67zl/+UCgYAU\n\
-Nasq3wQGef5yOrtVg2gRtgkr2GPNvHPL2Cm1/ePf3bfnRb6BjOW7SCLJ9xAQgd0Q\n\
-hEkhdgZfcfPykQKiAP4KMRwK8J4WVEP/Z8VlKFuPO1szQN0kq0xtpkHEIVgTQfK1\n\
-kl6B3mNZDqEcXJpP/yZ8YL5oSv52raPRSof+f9ExcQKBgCqVb/IS4nsvmgNzqfqg\n\
-VUdxsR9RmUSmrPeVJUOu0XzYR/RLBXM19XiOT78YRzAUWJjFZm7SoaZQcwLgGSw6\n\
-T0DtIPCMA4ld9UYknjMRSLBq1qt3u/DhkgWPvdARN9S4sqF+cQcNbN4CofpI5bdq\n\
-dYyMOVQz/HrNS/RTfTU54JbU\n\
+MIIJQwIBADANBgkqhkiG9w0BAQEFAASCCS0wggkpAgEAAoICAQCKRK4Ff7RkJ+1I
+pZWlsLjxZWL3aX0ySiONW604PsDi74cNNpLB7PbcFP3o0HieVf2n99+l3jUaVxYQ
+YxAJ3BKtXrTdQ/UWvoYUOZowIqnn2NEz7tUh/AxfqkN88z5OQz4MrwSEZ+Oh7hQw
+GwaFmLazFElwgI6gD8eopLA8fiRk+XKS6dhKs8tR4mDJe1yot67cW5vW6nOGuMyi
+aDlx6FkeNPjlyQ7BvLjGVt9XDyi1xPBmkPI+GXfAdtGKA7bOhmsvCbh/1gcXKOBe
+CSk3BpW9ZRnxr9M8lbZtlfsu3xX/AbX2g0Z+k3ABnCVcdV01uSZKUKsIzX1r50Hm
+Eo3BTn9Og2wYB6AY5YWTyGKTAz9V2szjMi/+395iaMLtk2+24/Q6eTCd6vPv8ijw
+mqNm429VLC2cvhDK2ll3sGMu+k7ragm7lLp0ydkaxvUi6IkWMoPks6r9TTwPgfwM
+uaSN9sb3uz5TlLivOYb1088nnMvcXsZ2GA9aBM6R6t8moXcEBobxip7xXQFKNa/w
+1nUX/d8Z3H9PTUuYwFvY8WOZqqSh2hyCFYFvzcSf0jZ9VM5cZxLNJDmrzNo3MCKc
+Ao0KJlzlIDGpkbdevXXCNGPoSWlTymwLQZ7F6L9QbJGynLo50gmMGZqz9pLlGRDX
+qSWEzWciPIbpBhWTCjglxsHiVhtvRQIDAQABAoICAAwWPuQqKrnKp7p/Bxrp3PD1
+PPaF2TTpODxmNDlDexcbe0HTcHbVYSsSBrQwbSriN39Ucs+MIjZAQKSEFGXYQCW5
+rrPc+fLYCt5/vpPQo+upj3gru6Px5Z8DQk0M8nhi9myjbBCrCEIijs85vZM2K1py
+Po1AH7esSXbblrBjEollKPfgy1CUcQSidnWGfC4fiICo3XGnIxw20WStGcB0YD38
+wXlM3yZeegSUmYeBwrxJD/7XUuXM0iQX/u9CrdtV8s602dUK/6mtoBd4U4sE31gk
+G2V+Skf0inL/9mo9hC27l42KIkJ0Xwk0/isb81775XX/oUPuurURDTiwjIXGd+hj
+PXsPxCs6VdPYb9GmCQRkWdT9Jlzu+tRDOSvPLKwa1L5CK9Pbm+31xAlxZ/w9jlOQ
+L9lo9a/GjSgxbt32uJXYpbSxu0HsAF+ZROH6VdFLTWJRenDOkNIPEON5zACe2eVA
+AskZ+EoAL9JCVsrflmQdRkhSsm/0VBtTb54i6UzsL6HxUOSLWSiYMG2Wu5xa9T35
+2AP8H6o7uVTbRRJR3u//rcF7Yv0I0J28kOa8Ld0tsXgNe1l3YG1Jse8Y7zU4yQXC
+h0YBMqKYUoLxC/aZhC2wpZqvSA1nt/OefwAVvC5fuCBxbEULd/ON/UX9h5pvs/BE
+o7lDLfIsMzg9+WbRBiChAoIBAQC+KRcxh1nF6ZrgfqpTIdZM9fMLzH+v7V1R6bNX
+ojn93SqemJb/kUGEIaubIQfJZcttKwmEWdyXHfDzH0MuoKz5XW5B47PAFWKGAroK
+QeQxy/Y9Gt5wjIKEfDvUjnmUvGSbizI5ReNMxR9/BZYmqIsR22jj+ZP6J/dq19Ze
+KcBULNNs7jV5IoM2nx9/nNYDQT/znqMhPTVadK/1Bo3mez6Vwo6wZz8k2hGXHWgd
+jJrFcc5epd+B7XxpDeesJdAtT3xz+HB/5MM6c8rLxrgR5QKUOUwh5Kjgj0bko5D4
+lDRELcfNrfENgal/4zOgVk/7qSIa2oLmiAgFzqWMgyc6CRh5AoIBAQC6JBzMTOTn
+hvZY0vgzPFtA4dN2iadu3lSruCi/EMVa8q80Rc4x3PVnMRXV7uHezexMVIAnTNDZ
+a+54q9crannnx0bHsTCD9vJQvm4WMCeKioxuN7iT9I+huAFkX0yxShFGCa4UvUml
+oHAKS5eFit2ruMyLlr/5lBMgDD97fvYlhmUIhF44P6gl8MneoiPs3yOL7u9IW3+f
+Hn+xQFT2dyU3fjpNtghF/9h9NAOyHRlHPzxk/sasszwuBmofKLSLQCWIBvkmK6EN
++RtmVt68G2NFsNUooYz6XTLYM4u8LVhcGgzikhMhruzG2p33T6HhrY7I4oRKN7ty
+/WNlpmjH7rItAoIBAQCj8ptbiV1d490yxiIlEFxJ+Ba2ynYgAJ7fe4Sy/lvPQrp3
+CAnPh7WbrMfeNGkZO1la4qsO38buxtKxWfe7IK5hRCtGRYH5Drbl4T3ykAKk6DuB
+TUQQdyQkA1Q2Gyw5jv+Slz0S4e1ph95yzic5Z0CXgbp6KvhoycJn7HSWtJpsOEWE
+eg/CQ34rPp+Sj254We5AJTpx8uqn9UirER6QYxt/VMFe13U6WYSm66STLWFW75rE
+QXBk/ZKwpFtkMSm9lvUuqzibG2kyYRir/cU/MFm733aDFjh/eyfdAiSAHI6/ZVUJ
+QHk+ctbnOsSM7T2CNL0Bt/j5trhsAVD7xyuvvp3JAoIBAQChZZiWyIGeAxbtM5hE
+DKxvGKAbYwYLirnN3zNtX+RPlKe34mMENzuNizEMu0Gbx9+A26/245MfQoeWmCFM
+otkq0E4d7hRMCD5ZJpUbpgtTBAj5tFTV7TLxHTQPzNYZz0gk+1W2493Mv80GED9E
+aoEEWYr56e9xPyRKIHSW5yIUrXBDL0rm6MMqipru8JXH2D7hIX7WtYd196LulQJW
+Zj6d3FQ/d3u6/ji/bu9ZbAO0FC+QvpyTuCGRIry3YbsSFr+0L5+uqhvOmtVYKjCc
+1/5+ufJJdbJgj5j7f85EjujTiz7q4Y+3IA5r6bkYbDLIFI3+vvHHzIU7ElwyElU+
+LkjNAoIBACrPxxQQnfMzSlXZ2G4uI5+Uypwy36ryUelydmDqZ7Z+wBXWmU4dfRgQ
+1xH10CCXza/orjhLWDcnPC1JYnE3KVS004S8/oHiLnjuUIvkP3QLovxx2BU6pNTe
+P3NC2LOwpSWg+h96PL2hXBe7zUxDf2WC0len6Xem/bcuBvYhCniqIf/aNwLsnh8g
+X7zWsXF01S/vtufsz1RoePedgKf1Xow+LflwxnDi1N623nHha/7nes9UVs3QAj3t
+tF2/jjT4Q1P2U1+4tmpKmS9izPMdEPVJLyrGuC0NuvLv4zwAmLBQbeuZh80L5eB7
+2QQagF6gXY9YhoPIYpzzqHW8PLz3kBU=\n\
 -----END PRIVATE KEY-----";
 
 int send_pending(int client_sock, struct TLSContext *context) {
@@ -90,7 +123,7 @@ struct HTTPInfo HTTPSClient(const char* website) {
     unsigned char read_buffer[0xFFFF];
     unsigned char client_message[0xFFFF];
 
-    int sockfd, portno = 443;
+    int sockfd, portno = 8080;
     struct sockaddr_in serv_addr;
     struct hostent *server;
     struct HTTPInfo info;
@@ -130,7 +163,7 @@ struct HTTPInfo HTTPSClient(const char* website) {
         tls_consume_stream(context, client_message, read_size, NULL);
         send_pending(sockfd, context);
         if (tls_established(context)) {
-            const char *request = "POST /growtopia/server_data.php HTTP/1.1\r\nUser-Agent: UbiServices_SDK_2019.Release.27_PC64_unicode_static\r\nHost: www.growtopia1.com\r\nAccept: */*\r\nConnection: close\r\ncontent-length: 0\r\n\r\n";
+            const char *request = "POST /growtopia/server_data.php HTTP/1.1\r\nUser-Agent: UbiServices_SDK_2022.Release.9_PC64_unicode_static\r\nHost: www.growtopia1.com\r\nAccept: */*\r\nConnection: close\r\ncontent-length: 0\r\n\r\n";
             if (!tls_make_ktls(context, sockfd)) send(sockfd, request, strlen(request), 0);
             else {
                 tls_write(context, (unsigned char *)request, strlen(request));
